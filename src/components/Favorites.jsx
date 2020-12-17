c92fdd080c22ae6714fec48f174d40f4118652ff
Celalalt666 <mihairaducroitoru@gmail.com>
2020-12-17 19:29:45 +0200

    My Favorites - page     displays corectly the products added in db

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,86 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import { axiosSpring } from '../common/axios';
+import React, { useState, useEffect } from "react";
+import { axiosSpring } from "../common/axios";
+import Meal from "./meals/Meal";
 
 export default function Favorites() {
-
   const [loading, setLoading] = useState(true);
-  const [mealsApi, setMealsApi] = useState();
+  const [mealsDb, setMealsDb] = useState();
 
-  const getFavoritesMealsApi = async () => {
+  const getFavoritesMealsDb = async () => {
     setLoading(true);
     const response = await axiosSpring
       .get(`/favorites`)
       .catch((err) => console.log("Error:", err));
     if (response && response.data) {
-    //   setMealsApi(response.data.meals);
-    console.log(response.data);
+      setMealsDb(response.data);
+      console.log(response.data.map((product) => product.id));
       setTimeout(() => {
         setLoading(false);
       }, 1300);
     }
   };
+
   useEffect(() => {
-    getFavoritesMealsApi();
-  }, []);
-
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-
-/**
- * import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { axios } from "../../common/axios";
-import Loading from "../loading/Loading";
-import Meal from "./Meal";
-
-const Meals = () => {
-  const [loading, setLoading] = useState(true);
-  const [mealsApi, setMealsApi] = useState();
-
-  const param = useParams();
-  const category = param.strCategory;
-
-  const getMealsApi = async () => {
-    setLoading(true);
-    const response = await axios
-      .get(`/filter.php?c=${category}`)
-      .catch((err) => console.log("Error:", err));
-    if (response && response.data) {
-      setMealsApi(response.data.meals);
-      setTimeout(() => {
-        setLoading(false);
-      }, 1300);
-    }
-  };
-  useEffect(() => {
-    getMealsApi();
+    getFavoritesMealsDb();
   }, []);
 
   if (loading) {
     return (
       <main>
-        <Loading />
+        <loading />
       </main>
     );
   }
 
   return (
     <>
-      <h3>{category}</h3>
       <div>
-        {mealsApi.map((meal) => {
-          return <Meal key={meal.idMeal} {...meal} />;
+        {mealsDb.map((product) => {
+          return (
+            <Meal
+              key={product.id}
+              idMeal={product.id}
+              strMeal={product.name}
+              strMealThumb={product.thumbnail}
+            />
+          );
         })}
       </div>
     </>
   );
-};
-
-export default Meals;
-
- */
+}
