964cc43f47c55a26425b3b7c7fa0fd10e00acd3d
Celalalt666 <mihairaducroitoru@gmail.com>
2020-12-17 19:58:42 +0200

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from "react";
-import {Redirect, Route, Switch} from "react-router-dom";
-import {ToastContainer} from "react-toastify";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import NotFound from "./components/NotFound";
 import NavBar from "./components/Navbar";
-import "react-toastify/dist/ReactToastify.css";
-import "./App.css";
 import Categories from "./components/category/Categories";
 import Meals from "./components/meals/Meals";
-import Favorites from './components/Favorites';
+import Favorites from "./components/Favorites";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
-    return (
-        <React.Fragment>
-            <ToastContainer/>
-            <NavBar/>
-            <main className="container">
-                <Switch>
-                    <Route path="/not-found" component={NotFound}/>
-                    <Route path="/categories" component={Categories}/>
-                    <Route path="/favorites" component={Favorites}/>
-                    <Route path="/meals/:strCategory" component={Meals} />
-                    <Redirect from="/" exact to="/categories"/>
-                    <Redirect to="/not-found"/>
-                </Switch>
-            </main>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <ToastContainer />
+      <NavBar/>
+      <main className="container">
+        <Switch>
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/categories" component={Categories} />
+          <Route path="/favorites" component={Favorites} />
+          <Route path="/meals/:strCategory" component={Meals} />
+          <Redirect from="/" exact to="/categories" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
