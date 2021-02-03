50c1dc3e7136d783560e60bff487ee676608503f
Alex <stanalexandrucode@gmail.com>
2021-02-03 14:03:01 +0200

    added prices & add to fav button

--- src/components/meals/DetailMeal.js
+++ src/components/meals/DetailMeal.js
@@ -1,73 +1,106 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { axios } from '../../common/axios';
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {axios, axiosSpring} from '../../common/axios';
 import Loading from '../loading/Loading';
 
 const DetailMeal = () => {
-  const [loading, setLoading] = useState(true);
-  const [detailMealApi, setDetailMealApi] = useState();
-  const [readMore, setReadMore] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [detailMealApi, setDetailMealApi] = useState();
+    const [readMore, setReadMore] = useState(false);
+    const [mealPrices, setMealPrices] = useState();
 
-  const param = useParams();
+    const param = useParams();
 
-  const getDetailMealApi = async () => {
-    setLoading(true);
-    const response = await axios
-      .get(`/lookup.php?i=${param.id}`)
-      .catch((err) => console.log('Error:', err));
-    if (response && response.data) {
-      console.log('ceva', response.data.meals[0]);
-      setDetailMealApi(response.data.meals[0]);
-      setLoading(false);
+    const getDetailMealApi = async () => {
+        setLoading(true);
+        const response = await axios
+            .get(`/lookup.php?i=${param.id}`)
+            .catch((err) => console.log('Error:', err));
+        if (response && response.data) {
+            console.log('ceva', response.data.meals[0]);
+            setDetailMealApi(response.data.meals[0]);
+            setLoading(false);
+        }
+    };
+
+    const getMealPrices = async () => {
+        const response = await axiosSpring
+            .get('/prices')
+            .catch((err) => console.log('Error:', err));
+        if (response && response.data) {
+            console.log('listapreturi', response);
+            setMealPrices(response.data);
+        }
+    };
+
+
+    const handleAdd = async () => {
+        await axiosSpring.post('/favorites', {
+            id: `${detailMealApi.idMeal}`,
+            price: `${prices}`,
+        });
+    };
+
+
+    useEffect(() => {
+        getDetailMealApi();
+        getMealPrices();
+    }, []);
+
+    if (loading) {
+        return (
+            <main>
+                <Loading/>
+            </main>
+        );
     }
-  };
-  useEffect(() => {
-    getDetailMealApi();
-  }, []);
+    let prices = mealPrices.filter((price) => {
+        return price.id === parseInt(detailMealApi.idMeal);
+    })[0].price;
 
-  if (loading) {
     return (
-      <main>
-        <Loading />
-      </main>
+        <div className="container">
+            <div className="header-detailMeal">
+                <div className="underline">
+                    <h3>{detailMealApi.strMeal}</h3>
+                </div>
+                <div className="detailMeal-page">
+                    <div>
+                        <img
+                            src={detailMealApi.strMealThumb}
+                            className="photo"
+                            alt={detailMealApi.strMeal}
+                        />
+                    </div>
+                    <div>
+                        <h5>Instructions:</h5>
+
+                        <p className="description-meal">
+                            {readMore
+                                ? detailMealApi.strInstructions
+                                : `${detailMealApi.strInstructions.substring(0, 200)}...`}
+                            <button
+                                className="showBtn"
+                                onClick={() => setReadMore(!readMore)}
+                            >
+                                {readMore ? 'show less' : 'read more'}
+                            </button>
+                            }
+                        </p>
+                        <h5>Area food: {detailMealApi.strArea}</h5>
+                        <p>Price {prices}$
+                        </p>
+
+                        <div>
+                            <button className="btn-meal" onClick={handleAdd}> add</button>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
     );
-  }
-
-  return (
-    <div className="container">
-      <div className="header-detailMeal">
-        <div className="underline">
-          <h3>{detailMealApi.strMeal}</h3>
-        </div>
-        <div className="detailMeal-page">
-          <div>
-            <img
-              src={detailMealApi.strMealThumb}
-              className="photo"
-              alt={detailMealApi.strMeal}
-            />
-          </div>
-          <div>
-            <h5>Instructions:</h5>
-
-            <p className="description-meal">
-              {readMore
-                ? detailMealApi.strInstructions
-                : `${detailMealApi.strInstructions.substring(0, 200)}...`}
-              <button
-                className="showBtn"
-                onClick={() => setReadMore(!readMore)}
-              >
-                {readMore ? 'show less' : 'read more'}
-              </button>
-              }
-            </p>
-            <h5>Area food: {detailMealApi.strArea}</h5>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default DetailMeal;
--- src/components/meals/Meals.js
+++ src/components/meals/Meals.js
@@ -39,7 +39,6 @@ const Meals = () => {
       price: `${price}`,
     });
     if (res.status !== 200) {
-      console.log(favorite);
     }
     setFavorite({ id: id, price: price });
   };
