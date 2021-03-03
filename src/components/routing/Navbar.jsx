51fcbc118fdfe9af1def055811ac91d6f565ec29
Mihaicv <mihaivoicu1977@gmail.com>
2021-03-03 19:54:22 +0200

    modified navbar

--- src/components/routing/Navbar.jsx
+++ src/components/routing/Navbar.jsx
@@ -4,70 +4,71 @@ import logo from '../../logo2.png';
 import Cookies from 'js-cookie';
 
 const Navbar = () => {
-  const [loggedUser, setLoggedUser] = useState('empty');
+    const [loggedUser, setLoggedUser] = useState('empty');
 
-  // const [userName, setUserName] = useContext(AuthContext);
+    // const [userName, setUserName] = useContext(AuthContext);
 
-  // console.log('object');
-  // const readCookie = () => {
-  //   const user = Cookies.get('name');
-  //   if (user) {
-  //     setLoggedUser(user);
-  //   }
-  // };
+    // console.log('object');
+    // const readCookie = () => {
+    //   const user = Cookies.get('name');
+    //   if (user) {
+    //     setLoggedUser(user);
+    //   }
+    // };
 
-  useEffect(() => {
-    // readCookie();
-    // console.log(userName);
-  }, [loggedUser]);
+    useEffect(() => {
+        // readCookie();
+        // console.log(userName);
+    }, [loggedUser]);
 
-  return (
-    <nav className="navbar">
-      <Link className="navbar-brand" to="/">
-        <img className="img-logo" src={logo} alt="site logo" />
-      </Link>
-      <NavLink className="navbar-brand" to="/favorites">
-        my favorites
-      </NavLink>
-      {/*<NavLink className="navbar-brand" to="/recipes/addRecipe">*/}
-      {/*  add recipe*/}
-      {/*</NavLink>*/}
-        <NavLink className="navbar-brand" to="/recipes/all">
-        recipes
-      </NavLink>
-      {/* <NavLink className="navbar-brand" to="/login">
-        login
-      </NavLink> */}
-      <NavLink className="navbar-brand" to="/register">
-        register
-      </NavLink>
-      <Link className="navbar-brand" to="/logout">
-        logout
-      </Link>
-      <NavLink className="navbar-brand" to="/cart">
-        cart
-      </NavLink>
-        <div>
-        {Cookies.get('token')?
-        <div className="navbar-brand name">Welcome, you are logged as {Cookies.get('name')}</div>: null
-        }
-        </div>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      {/*<Link className="navbar-brand">*/}
-      {/*  {loggedUser !== 'empty' ? Cookies.get('name') : 'not logged in'}*/}
-      {/*</Link>*/}
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <Link className="navbar-brand" to="/">
+                <img className="img-logo" src={logo} alt="site logo"/>
+            </Link>
+
+            <NavLink className="navbar-brand" to="/recipes/all">
+                recipes
+            </NavLink>
+
+
+            {!Cookies.get('token') ?
+                <div>
+                    <NavLink className="navbar-brand" to="/register">
+                        register/login
+                    </NavLink>
+
+                </div>
+                :
+                <div>
+                    <NavLink className="navbar-brand" to="/favorites">
+                        my favorites
+                    </NavLink>
+                    <NavLink className="navbar-brand" to="/cart">
+                        cart
+                    </NavLink>
+                    <Link className="navbar-brand" to="/logout">
+                        logout
+                    </Link>
+                    <div className="navbar-brand name">Welcome, you are logged as {Cookies.get('name')}</div>
+                </div>
+            }
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            {/*<Link className="navbar-brand">*/}
+            {/*  {loggedUser !== 'empty' ? Cookies.get('name') : 'not logged in'}*/}
+            {/*</Link>*/}
+        </nav>
+    );
 };
 
 export default Navbar;
