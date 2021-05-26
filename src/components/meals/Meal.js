80915e408aae902915186cbe2c76cb6aba801d6a
Celalalt666 <mihairaducroitoru@gmail.com>
2021-05-26 20:26:09 +0300

    add to fav btn and add to cart while not logged in

--- src/components/meals/Meal.js
+++ src/components/meals/Meal.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
 import {Link} from "react-router-dom";
 import "./Meal.css"
+import {AuthContext} from "../authentication/AuthContext";
 
 const Meal = ({idMeal, strMeal, strMealThumb, price, handleAddToFav, handleAddToCart}) => {
 
+    const {auth} = useContext(AuthContext);
 
+    useEffect(() => {
+
+    }, [auth])
     return (
         <>
             <div className='meal'>
-                <div >
+                <div>
                     <Link to={`/meal/${idMeal}`}>
                         <h3 className="text-meal">{strMeal}</h3>
                     </Link>
@@ -16,13 +21,20 @@ const Meal = ({idMeal, strMeal, strMealThumb, price, handleAddToFav, handleAddTo
                 <div>
                     <div className="price-add-meal">
                         <Link to={`/meal/${idMeal}`}>
-                        <img className='photo' src={strMealThumb} alt={strMeal}/>
+                            <img className='photo' src={strMealThumb} alt={strMeal}/>
                         </Link>
                         <p className="price">Price ${price}</p>
-                        <div>
-                            <button className="btn-meal" onClick={() => handleAddToFav(idMeal, price)}> add fav</button>
-                            <button className="btn-addCart" onClick={() => handleAddToCart(idMeal, price)}>Add to cart</button>
-                        </div>
+                        {auth ? (
+                            <div>
+                                <button className="btn-meal" onClick={() => handleAddToFav(idMeal, price)}> add fav
+                                </button>
+                                <button className="btn-addCart" onClick={() => handleAddToCart(idMeal, price)}>Add to
+                                    cart
+                                </button>
+                            </div>
+                        ) : (
+                            <div/>
+                        )}
                     </div>
                 </div>
             </div>
